c91cce8c091e628fc5891992c7ea8cf75eb27344
Zekkruum <juancamilogarciabonilla54@gmail.com>
2025-12-15 05:33:32 +0000

    ahora si es hora de deployntear

--- config.py
+++ config.py
@@ -1,18 +1,23 @@
+import os
+from dotenv import load_dotenv
+
+load_dotenv()  # 👈 ANTES de usar getenv
+
 # ----------------------------
 # Configuración de la base de datos
 # ----------------------------
 
-import os
-import mysql.connector
+DB_CONFIG = {
+    "host": os.getenv("DB_HOST"),
+    "port": int(os.getenv("DB_PORT")),
+    "user": os.getenv("DB_USER"),
+    "password": os.getenv("DB_PASSWORD"),
+    "database": os.getenv("DB_NAME"),
+    "ssl_disabled": True
+}
 
-mysql.connector.connect(
-    host=os.getenv("DB_HOST"),
-    port=int(os.getenv("DB_PORT")),
-    user=os.getenv("DB_USER"),
-    password=os.getenv("DB_PASSWORD"),
-    database=os.getenv("DB_NAME"),
-    ssl_disabled=True
-)
+print("DB_PORT:", os.getenv("DB_PORT"))
+
 
 
 #-----------------------------
