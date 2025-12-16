78b2a44c4fd1a20fdf4c9cc383f346172b0c5b81
Zekkruum <juancamilogarciabonilla54@gmail.com>
2025-12-16 15:35:43 -0500

    Delete start.sh

--- start.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-# Activar el entorno virtual
-source venv/bin/activate
-# Comando encargado de instalar dependecias y librerias
-pip install -r requeriments.txt
-# Iniciar la aplicación
-python app.py
