c9f617d5a26ecd6fd8b7dcd403d868af9a19b6aa
Zekkruum <juancamilogarciabonilla54@gmail.com>
2025-12-16 15:26:50 -0500

    Fix pip command and Python executable usage

--- start.sh
+++ start.sh
@@ -2,6 +2,6 @@
 # Activar el entorno virtual
 source venv/bin/activate
 # Comando encargado de instalar dependecias y librerias
-pip3 install -r requeriments.txt
+pip install -r requeriments.txt
 # Iniciar la aplicación
-python3 app.py
+python app.py
