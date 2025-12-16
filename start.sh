83349dd109f92edd5168787b3a8e4192355e9bc3
Zekkruum <juancamilogarciabonilla54@gmail.com>
2025-12-16 15:12:40 -0500

    modificacion del start.sh para instalar dependencias xd
    Add command to install dependencies from requirements.txt

--- start.sh
+++ start.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 # Activar el entorno virtual
 source venv/bin/activate
+# Comando encargado de instalar dependecias y librerias
+pip install -r requeriments.txt
 # Iniciar la aplicación
 python app.py
